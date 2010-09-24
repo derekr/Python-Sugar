c9a4e24e5309eb58aa208f52c4e5ca137eaab4ec
Derek Reynolds <derekr@me.com>
2010-09-23 22:03:24 -0700

    First commit.

---
+++ DRPythonClass.h
@@ -0,0 +1,16 @@
+//
+//  DRPythonClass.h
+//  PythonSugar
+//
+//  Created by Derek Reynolds on 9/23/10.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <EspressoItemizer.h>
+
+
+@interface DRPythonClass : ESBaseItem {
+	NSString *name;
+}
+
+@end
---
+++ DRPythonClass.m
@@ -0,0 +1,14 @@
+//
+//  DRPythonClass.m
+//  PythonSugar
+//
+//  Created by Derek Reynolds on 9/23/10.
+//  Copyright 2010 whatupderek.com. All rights reserved.
+//
+
+#import "DRPythonClass.h"
+
+
+@implementation DRPythonClass
+
+@end
